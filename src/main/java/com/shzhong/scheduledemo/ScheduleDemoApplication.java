36eb81cc9eed3d77a599f012cdc0712f4bd64828
chitzzz <648267970@qq.com>
2018-12-05 22:59:31 +0800

    first commit

---
+++ src/main/java/com/shzhong/scheduledemo/ScheduleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shzhong.scheduledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class ScheduleDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleDemoApplication.class, args);
+    }
+}
